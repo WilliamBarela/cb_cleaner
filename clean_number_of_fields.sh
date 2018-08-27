68dbafafe7138412afecff6af0f0b92021757208
William Barela <william.barela.dev@gmail.com>
2018-08-26 22:18:35 -0500

    separates by number of fields

--- clean_number_of_fields.sh
+++ clean_number_of_fields.sh
@@ -1,5 +1,5 @@
 for file in $(ls *.csv); do
-  nof=$(awk 'NR == 1' people.csv | awk -F, '{print NF}');
-  # awk -vFPAT='([^,]*)|("[^"]+")' -vOFS=, 'NF!=17{print FNR,NF,$0}' people.csv > ./output/not17Fpeopletest.csv;
-  awk -v var=$nof -vFPAT='([^,]*)|("[^"]+")' -vOFS=, 'NF!=var{print FNR,NF,$0}' people.csv > ./output/not17Fpeopletest.csv;
+  nof=$(awk 'NR == 1' $file | awk -F, '{print NF}');
+  awk -v var=$nof -vFPAT='([^,]*)|("[^"]+")' -vOFS=, 'NF!=var{print FNR,NF,$0}' $file > ./bad_nof_output/"${file%.*}".csv;
+  awk -v var=$nof -vFPAT='([^,]*)|("[^"]+")' -vOFS=, 'NF==var{print $0}' $file > ./clean_nof_output/"${file%.*}".csv;
 done
