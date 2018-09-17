972fdedd1c21ec7aa86e400e4fb0dae62b7508cf
William <william.barela.dev@gmail.com>
2018-09-16 23:47:57 -0500

    Create find_tickers_in_ipos.sh

---
+++ find_tickers_in_ipos.sh
@@ -0,0 +1,14 @@
+# pull select data from ipos.csv for each ticker that matches from tickers
+for i in $(seq 1 $(wc -l tickers | awk '{print $1}')); do 
+  key=$(sed -n $i"p" tickers);
+  awk -v var=$key -F, -vOFS=, '$7 ~ var{print $7,$6,var,$15,$2,$3,$4,$5,$1}' ipos.csv | grep -iwP "^\"$key\"" >> matches.csv;
+done
+
+# add header to file:
+echo '"ipos_ticker","stock_echange_symbol","tickers_from_list","org_uuid","country_code","company_state_code","region","city","company_name"' > matches_final.csv
+
+# remove duplicates
+sort matches.csv | uniq >> matches_final.csv
+
+# useful code for counting number of matches for each key: you need to change names of files.
+# for i in $(seq 1 $(wc -l ticker_name | awk '{print $1}')); do key=$(sed -n $i"p" ticker_name); num_matches=$(grep -icw "$key" org_uuid.csv); echo $num_matches,$key >> num_matches.csv; done
